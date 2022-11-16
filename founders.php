65e6702669e0eabbd11aa7aa1da1eee555b480ee
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-11-16 13:45:24 -0600

    Add files via upload

--- founders.php
+++ founders.php
@@ -5,15 +5,50 @@
 
 <?php require_once("header.php")?>
 
+    <!-- Div which is 100% of page -->
+    <div id='founderBackDiv'>
+
+        <!-- Top 1/2 of page -->
+        <div>
+
+        
+        </div>
+
+        <!-- bottom 1/2 of page -->
+        <div>
+
+            <!--About Nicole -->
+            <div>
+            
+                <!-- Image -->
+                <div>
+                    <img src="https://media-exp1.licdn.com/dms/image/C5603AQFvAaQW30mjag/profile-displayphoto-shrink_400_400/0/1629331853308?e=1674086400&v=beta&t=z62IE1kzoF_2Y7N9iEL5oxM1gHSzvdnoPFrysVoxZ3g" alt="Nicole">
+                </div>
+                
+                <div>
+
+                </div>
+
+            </div>
+
+            <!--About Darren -->
+            <div>
+
+                <!-- Image -->
+                <div>
+                    <img src="https://media-exp1.licdn.com/dms/image/C4E03AQEXLkjEQK7QYg/profile-displayphoto-shrink_400_400/0/1621025060836?e=1674086400&v=beta&t=8iPq0SLTLrraWpaoK4p6oY-euPQlglgDJSPuNzjlu0c" alt="Darren">
+                </div>
+
+                <div>
+
+                </div>
 
 
+            </div>
 
+        </div>
 
-
-
-
-
-
+    <div>
 
     </body>
 </html>
