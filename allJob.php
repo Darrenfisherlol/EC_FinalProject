4a2f29b6f122b99e9fa75823226f347bf6aabdfe
Nicole Goral <78056391+NicoleGoral@users.noreply.github.com>
2022-11-16 14:23:56 -0600

    Update allJob.php

--- allJob.php
+++ allJob.php
@@ -5,15 +5,52 @@
 
 <?php require_once("header.php")?>
 
+<div>
 
+    <tbody>
+    <?php
+    $servername = "localhost";
+    $username = "ngoralou_finalProject";
+    $password = "DarrenNicole1";
+    $dbname = "ngoralou_finalProject";
 
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
 
+    $jid = $_GET["id"];
+    $sql = "SELECT jobID, title, link, companyName, location, hiringTime, listedDate, listedTime FROM allJob j Where jobID =" . $jid;
+    $result = $conn->query($sql);
 
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+        ?>
+    <tr>
+      <td><?=$row["jobID"]?></td>
+      <td><?=$row["title"]?></td>
+      <td><?=$row["companyName"]?></td>
+      <td><?=$row["location"]?></td>
+      <td><?=$row["hiringTime"]?></td>
+      <td><?=$row["listedDate"]?></td>
+      <td><?=$row["listedTime"]?></td>
+    </tr>
+      <?php
+        }
+    } else {
+      echo "0 results";
+    }
+    $conn->close();
+    ?>     
+      </tbody>
+    </table>
 
+</div>
 
-
-
-
-
-    </body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
 </html>
+
