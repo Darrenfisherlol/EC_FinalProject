af3611c7b7579c4698872f27686e4d106993f5c5
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-12-05 13:24:36 -0600

    Add files via upload

--- founders.php
+++ founders.php
@@ -6,66 +6,61 @@
 <?php require_once("header.php")?>
 
     <!-- Div which is 100% of page -->
-    <div id='founderBackDiv'>
+    <div>
 
         <!-- Top 1/2 of page -->
         <div style="width: 100%; height:75px">
-                <!-- Info about founders -->
-                <div>
-                    <p>
-                        Nicole and Darren worked with the University of Oklahoma, Dr. Bellah, and OU create to make all their dreams come true. 
-                        Jobly is their beautiful brain child that they hope will help future students and job seekers to stay organized and eventually find a job.
-                        They hope that you have a wonderful experience using Jobly!
-                    </p>
-                </div>
+            <!-- Info about founders -->
+            <div>
+                <p>
+                    Nicole and Darren worked with the University of Oklahoma, Dr. Bellah, and OU create to make all their dreams come true. 
+                    Jobly is their beautiful brain child that they hope will help future students and job seekers to stay organized and eventually find a job.
+                    They hope that you have a wonderful experience using Jobly!
+                </p>
+            </div>
+        </div>
+
+    
+        <!--About Nicole -->
+        <div style="width: 100%; height:200px">
         
-        </div>
-
-        <div>
-
-            <!--About Nicole -->
-            <div style="width: 100%; height:200px">
+            <!-- Image -->
+            <div style="width: 50%; float: right"> 
+                <img src="https://media-exp1.licdn.com/dms/image/C5603AQFvAaQW30mjag/profile-displayphoto-shrink_400_400/0/1629331853308?e=1674086400&v=beta&t=z62IE1kzoF_2Y7N9iEL5oxM1gHSzvdnoPFrysVoxZ3g" alt="Nicole">
+            </div>
             
-                <!-- Image -->
-                <div style="width: 50%; float: right"> 
-                    <img src="https://media-exp1.licdn.com/dms/image/C5603AQFvAaQW30mjag/profile-displayphoto-shrink_400_400/0/1629331853308?e=1674086400&v=beta&t=z62IE1kzoF_2Y7N9iEL5oxM1gHSzvdnoPFrysVoxZ3g" alt="Nicole">
-                </div>
-                
-                <div style="width: 50%; float: right"> 
+            <div style="width: 50%; float: right"> 
 
-                    <p>
-                        Nicole Goral is a senior studying Management Information Systems and International Business. 
-                        After applying to over 70 jobs, Nicole got overwhelmed with tracking which jobs she applied to. 
-                        This website is to help people like Nicole who want to find a job and stay organized while doing so!
-                    </p>
-
-                </div>
-
-            </div>
-
-            <!--About Darren -->
-            <div style="width: 100%; height:200px">
-
-                <!-- Image -->
-                <div style="width: 50%; float: right">
-                    <img src="https://media-exp1.licdn.com/dms/image/C4E03AQEXLkjEQK7QYg/profile-displayphoto-shrink_400_400/0/1621025060836?e=1674086400&v=beta&t=8iPq0SLTLrraWpaoK4p6oY-euPQlglgDJSPuNzjlu0c" alt="Darren">
-                </div>
-
-                <div style="width: 50%; float: right">
-
-                    <p>
-                        Darren Fisher is a senior studying Management Information Systems and is on the accelerated Masters track in Information Technology. 
-                        Darren is constantly trying to create the next big thing or work with startups. 
-                        Darren loves helping his friends and creating solutions to problems even more!
-                    </p>
-
-                
-                </div>
-
+                <p>
+                    Nicole Goral is a senior studying Management Information Systems and International Business. 
+                    After applying to over 70 jobs, Nicole got overwhelmed with tracking which jobs she applied to. 
+                    This website is to help people like Nicole who want to find a job and stay organized while doing so!
+                </p>
 
             </div>
 
         </div>
+
+        <!--About Darren -->
+        <div style="width: 100%; height:200px">
+
+            <!-- Image -->
+            <div style="width: 50%; float: right">
+                <img src="https://media-exp1.licdn.com/dms/image/C4E03AQEXLkjEQK7QYg/profile-displayphoto-shrink_400_400/0/1621025060836?e=1674086400&v=beta&t=8iPq0SLTLrraWpaoK4p6oY-euPQlglgDJSPuNzjlu0c" alt="Darren">
+            </div>
+
+            <div style="width: 50%; float: right">
+
+                <p>
+                    Darren Fisher is a senior studying Management Information Systems and is on the accelerated Masters track in Information Technology. 
+                    Darren is constantly trying to create the next big thing or work with startups. 
+                    Darren loves helping his friends and creating solutions to problems even more!
+                </p>
+            </div>
+
+        </div>
+
+        
 
     <div>
 
