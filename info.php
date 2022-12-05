8c5d7aa808a03d9be8ebd302c4f1114316100683
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-12-05 12:57:28 -0600

    Add files via upload

--- info.php
+++ info.php
@@ -8,27 +8,66 @@
     <!-- Div which is 100% of page -->
     <div>
 
-        <div style="height: 100px; width: 200px; background-color: grey;">
+        <div style="height: 25%; width: 100%; background-color: grey;">
 
-            box 1
+            <div style="height: 75%; width: 100%; background-color: lightblue;">
+                
+                box 1.a
+                header
+
+            </div>
+
+            <div style="height: 25%; width: 100%; background-color: lightgreen;">
+                
+                box 1.b
+                Jobly
+
+            </div>
 
         </div>
 
-        <div style="height: 100px; width: 200px; background-color: lightblue;">
+        <div style="height: 25%; width: 100%; background-color: lightblue;">
 
-            box 2
+        
+
+            <div style="float: left; width: 50%; padding: 5px;">
+                
+                box 2.a
+                Jobly was helped me get 100 job interviews with 100% success
+
+            </div>
+
+            <div style="float: left; width: 50%; padding: 5px;">
+                
+                box 2.b
+                Jobly was created in 2022 as the increasing need for a job after graduation became more prevalent
+
+            </div>
 
         </div>
 
-        <div style="height: 100px; width: 200px; background-color: lightred;">
+        <div style="height: 25%; width: 100%; background-color: lightred;">
 
-            box 3
+            <div style="float: left; width: 50%; padding: 5px;">
+                
+                box 3.a
+                traditional sources like linkedin... dont work
+
+            </div>
+
+            <div style="float: left; width: 50%; padding: 5px;">
+                
+                box 3.b
+                they are a collection of jobs for everyone... but none for you
+
+            </div>
 
         </div>
 
-        <div style="height: 100px; width: 200px; background-color: lightgreen;">
+        <div style="height: 25%; width: 100%; background-color: lightgreen;">
 
             box 4
+            Founder page
 
         </div>
 
