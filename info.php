1aebf27decd52ed72fb1b87981b8c052d5ce2cbf
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-12-05 12:37:17 -0600

    Add files via upload

--- info.php
+++ info.php
@@ -13,14 +13,14 @@
 
             <div>
             
-                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcoed.com%2Fwp-content%2Fuploads%2F2018%2F02%2Fshutterstock_445183672.jpg&f=1&nofb=1&ipt=4b4c7fc52061d9bb89e94ff1b4f13cd65ecbd485247fdc952c4217930c5298d4&ipo=images" alt="OU">
+                <img style="height: 20%; width: 100%;" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcoed.com%2Fwp-content%2Fuploads%2F2018%2F02%2Fshutterstock_445183672.jpg&f=1&nofb=1&ipt=4b4c7fc52061d9bb89e94ff1b4f13cd65ecbd485247fdc952c4217930c5298d4&ipo=images" alt="OU">
 
                 
 
             </div>
             
-            <div>
-                <p >
+            <div style="height: 100px; height: 100px">
+                <p>
                     Jobly
                 </p>
             </div>
@@ -30,14 +30,14 @@
 
         <!-- bottom 2/4 of page -->
         <div>
-            <div>
+            <div style="background-color: lightcoral;"
                 <p>
                     "Jobly was helped me get 100 job interviews with 100% success"
                 </p>
 
             </div>
 
-            <div>
+            <div style="float: right; background-color: lightblue;">
             
                 <p >
 
@@ -54,37 +54,38 @@
         <div>
 
             
-            <div>
+            <div style="float: right;">
 
                 <p>
                     Jobly was created in 2022 as the increasing need for a job after graduation became more prevalent
                 </p>
                 
             </div>
+
             <div>
 
-                <p>
+                <p style="float: right;">
 
                     <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fyt3.ggpht.com%2Fa%2FAGF-l78UKMjzMY_5PjOE63fAbRCCkbFLq_sykeDdcw%3Ds900-c-k-c0xffffffff-no-rj-mo&f=1&nofb=1&ipt=f0ec152fcbbd599a0c19dbe59e689012042a9574f1946ef81862891048f9e0a2&ipo=images" alt="linkein">
                     
                 </p>
-                <p>
+                <p style="float: right;">
 
                     traditional sources like linkedin... dont work
 
                 </p>
-                <p>
+                <p style="float: right;">
                     <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2Fd6%2Ff9%2Ff3%2Fd6f9f3a697627118e67439b246f05f64.jpg&f=1&nofb=1&ipt=3af63d5aff6fa05a4fe36e4658716fe53ba2c52101b9ec58640be3db62c7afc7&ipo=images">
                 </p>
-                <p>
+                <p style="float: right;">
                     From our research of two people, we noticed a lack of success with other platform. 
                 </p> 
-                <p>
+                <p style="float: right;">
                     Jobly aims to get it done.
                 </p>
             </div>
 
-            <div>
+            <div style="background-color: lightpink; float: right">
 
                 Image about job search 
 
@@ -93,12 +94,12 @@
         <!-- bottom 4/4 of page -->
         <div>
 
-            <div>
+            <div style="background-color: lightred;">
 
                 imager - to foudners page link image click
 
             </div>
-            <div >
+            <div style="background-color: lightblue; float: right">
 
                  <p>
 
