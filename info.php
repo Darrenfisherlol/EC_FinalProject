dcc7cdcad650d7138cba5a4f0794e91efc7daa97
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-12-05 12:49:14 -0600

    Add files via upload

--- info.php
+++ info.php
@@ -8,101 +8,31 @@
     <!-- Div which is 100% of page -->
     <div>
 
-        <!-- Top 1/4 of page -->
-        <div">
+        <div style="height: 100px; width: 200px; background-color: grey;">
 
-            <div>
-            
-                <img style="height: 100px; width: 200px;" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcoed.com%2Fwp-content%2Fuploads%2F2018%2F02%2Fshutterstock_445183672.jpg&f=1&nofb=1&ipt=4b4c7fc52061d9bb89e94ff1b4f13cd65ecbd485247fdc952c4217930c5298d4&ipo=images" alt="OU">
-
-                
-
-            </div>
-            
-            <div style="font-size:50px; height: 100px; height: 100px; float: left;">
-                <p>
-                    Jobly
-                </p>
-            </div>
-            
+            box 1
 
         </div>
 
-        <!-- bottom 2/4 of page -->
-        <div>
-            <div style="background-color: lightcoral; float: left;">
-                <p>
-                    "Jobly was helped me get 100 job interviews with 100% success"
-                </p>
-            </div>
+        <div style="height: 100px; width: 200px; background-color: lightblue;">
 
-            <div style="float: left; background-color: lightblue;">
-            
-                <p >
-                    Text about - why
-                </P>
-            </div>
+            box 2
 
         </div>
 
-        <!-- bottom 3/4 of page -->
-        <div>            
-            <div style="float: left;">
+        <div style="height: 100px; width: 200px; background-color: lightred;">
 
-                <p>
-                    Jobly was created in 2022 as the increasing need for a job after graduation became more prevalent
-                </p>
-            </div>
-
-            <div>
-
-                <p style="font-size:50px; width: 100px; height: 100px; float: right;">
-
-                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fyt3.ggpht.com%2Fa%2FAGF-l78UKMjzMY_5PjOE63fAbRCCkbFLq_sykeDdcw%3Ds900-c-k-c0xffffffff-no-rj-mo&f=1&nofb=1&ipt=f0ec152fcbbd599a0c19dbe59e689012042a9574f1946ef81862891048f9e0a2&ipo=images" alt="linkein">
-                    
-                </p>
-                <p style="float: right;">
-
-                    traditional sources like linkedin... dont work
-
-                </p>
-                <p style="font-size:px; width: 100px; height: 75px; float: right;">
-                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2Fd6%2Ff9%2Ff3%2Fd6f9f3a697627118e67439b246f05f64.jpg&f=1&nofb=1&ipt=3af63d5aff6fa05a4fe36e4658716fe53ba2c52101b9ec58640be3db62c7afc7&ipo=images">
-                </p>
-                <p style="float: right;">
-                    From our research of two people, we noticed a lack of success with other platform. 
-                </p> 
-                <p style="float: right;">
-                    Jobly aims to get it done.
-                </p>
-            </div>
-
-            <div style="background-color: lightpink; float: right">
-
-                Image about job search 
-
-            </div>
-        </div>
-        <!-- bottom 4/4 of page -->
-        <div>
-
-            <div style="background-color: lightred; float: right;">
-
-                imager - to foudners page link image click
-
-            </div>
-            <div style="background-color: lightblue; float: right">
-
-                 <p>
-
-                    Text about - who
-                    link to us - foudners page
-                </P>
-
-            </div>
+            box 3
 
         </div>
-    <div>
+
+        <div style="height: 100px; width: 200px; background-color: lightgreen;">
+
+            box 4
+
+        </div>
+
+    </div>
 
     </body>
 </html>
