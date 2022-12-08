18e34eed4cc97610776c68c6a637b9a24629e6da
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-12-08 13:34:21 -0600

    Add files via upload

--- info.php
+++ info.php
@@ -12,11 +12,11 @@
 
             <div style="height: 100px; width: 100%; background-color: lightblue; float: left;">
                 
-                <img style="width: 100px; height: 100px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcoed.com%2Fwp-content%2Fuploads%2F2018%2F02%2Fshutterstock_445183672.jpg&f=1&nofb=1&ipt=4b4c7fc52061d9bb89e94ff1b4f13cd65ecbd485247fdc952c4217930c5298d4&ipo=images" alt="OU">
+                <img style="width: 100%; height: 300px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcoed.com%2Fwp-content%2Fuploads%2F2018%2F02%2Fshutterstock_445183672.jpg&f=1&nofb=1&ipt=4b4c7fc52061d9bb89e94ff1b4f13cd65ecbd485247fdc952c4217930c5298d4&ipo=images" alt="OU">
 
             </div>
 
-            <div style="text-align: center; font-size: 50px; height: 25px; width: 100%; background-color: lightred; float: left;">
+            <div style="text-align: center; font-size: 50px; height: 25px; width: 100%; background-color: #fef1e4; float: left; color: #372823">
                 
                 Jobly
 
@@ -24,17 +24,22 @@
 
         </div>
 
-        <div style="height: 250px; width: 100%; background-color: lightcoral;">
+        <div style="height: 250px; width: 100%; background-color: #fef1e4;">
 
         
 
-            <div style="float: left; height:100%; width: 50%; padding: 5px; text-align: center; font-size:25px; background-color: lightcoral;">
+            <div style="float: left; height:100%; width: 50%; padding: 5px; text-align: center; font-size:25px; background-color: #fde3c8;">
                 
-                  Jobly has helped me get 100 job interviews with 100% success
+                <p style="font-size:50px; color: #a3b6bc" >
+                    Customer Experience
+                <p>
+                <p style="font-size:50px; color:#a3b6bc" >
+                    Jobly has helped me get 100 job interviews with 100% success
+                <p>
 
             </div>
 
-            <div style="float: left; height:100%; width: 50%; padding: 5px; text-align: center; font-size:25px; background-color: lightpink;">
+            <div style="float: left; height:100%; width: 50%; padding: 5px; text-align: center; font-size:25px; background-color: #fef1e4;">
                 
                 
                 Jobly was created in 2022 as the increasing need for a job after graduation became more prevalent
@@ -45,22 +50,27 @@
 
         <div style="height: 250px; width: 100%;">
 
-            <div style="float: left; height:100%; width: 50%; padding: 5px; background-color: lightred; text-align: center; font-size:25px;">
+            <div style="float: left; height:100%; width: 50%; padding: 5px; background-color: #fef1e4; text-align: center; ">
                 
-                
-                Traditional sources like linkedin... Don't Work
+                <p style="font-size:25px;">
+                    Traditional sources like linkedin... Don't Work
+                </p>
 
+                <div>
+                    <img width="25%" height="150" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fblog.carltonstaffing.com%2Fwp-content%2Fuploads%2F2020%2F09%2Flinkedin-icon-logo-png-transparent-2048x2048.png&f=1&nofb=1&ipt=e32db8beb9891a030a378a029b8454bdfee8302fd7e4845b708a1cf0b8f767cd&ipo=images">
+                </div>
             </div>
 
-            <div style="float: left; height:100%; width: 50%; padding: 5px; background-color: lightyellow; text-align: center; font-size:25px;">
+            <div style="float: left; height:100%; width: 50%; padding: 5px; background-color: #fef1e4; text-align: center; font-size: 50px; color:#372823; margin-top: 50px">
                 
                 
-                They are a collection of jobs for everyone... 
+                Sights like Linkedin collect user information and use it to make you buy expensive addons for +$30 a month!
+                
             </div>
 
         </div>
 
-        <div style="text-align: center; font-size:50px; height: 250px; width: 100%; background-color: lightgreen;">
+        <div style="text-align: center; font-size:50px; height: 50px; width: 100%; background-color: #fde3c8;">
 
             
             Founder page
