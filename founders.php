8e60238d6bc0433b2348bfae3c2fe0425fb4e480
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-12-08 20:43:23 -0600

    Add files via upload

--- founders.php
+++ founders.php
@@ -8,33 +8,33 @@
     <!-- Div which is 100% of page -->
     <div style="background-color: #fef1e4;">
 
-        <div style="Width:100%; height:100px; margin-top: 25px; margin-bottom: 20px; text-align: center; font-weight: bold; font-size:75px ">
+        <div style="Width:100%; height:100px; margin-bottom: 10px; text-align: center; font-weight: bold; font-size:75px ">
             <p style="font-size:50px; color: #372823; background-color: #fde3c8;">
                 About Founders:
             </p>
         </div>
 
-        <div style="width: 100%; height:175px; margin-bottom: 50px; background-color: #a3b6bc; color:#372823 ">
+        <div style="width: 100%; height:175px; margin-bottom: 10px;; background-color: #fde3c8; color:#f0913d ">
             
-            <div style="width: 31%; float: left;font-size:25px; text-align:center; margin-left:15px; margin-right:15px; background-color: #f0913d;">
+            <div style="width: 31%; float: left;font-size:25px; text-align:center; margin-left:15px; margin-right:15px; background-color: #fef1e4;">
                 Nicole and Darren worked with the University of Oklahoma, Dr. Bellah, and OU create to make all their dreams come true.
             </div>
-            <div style="width: 31%; float: left;font-size:25px; text-align:center; margin-left:15px; margin-right:15px;background-color: #f0913d;">
+            <div style="width: 31%; float: left;font-size:25px; text-align:center; margin-left:15px; margin-right:15px;background-color: #fef1e4;">
                 Jobly is their beautiful brain child that they hope will help future students and job seekers to stay organized and eventually find a job.
             </div>
-            <div style="width: 31%; float: left;font-size:25px; text-align:center; margin-left:15px; margin-right:15px;background-color: #f0913d;">
+            <div style="width: 31%; float: left;font-size:25px; text-align:center; margin-left:15px; margin-right:15px;background-color: #fef1e4;">
                 They hope that you have a wonderful experience using Jobly!
             </div>
           
         </div>
          
-        <div style="width: 100%; height:300px;  margin-top: 25px; margin-bottom: 25px; background-color: #fde3c8; color:#372823; ">
+        <div style="width: 100%; height:300px; margin-bottom: 10px; background-color: #fde3c8; color:#372823; ">
         
-            <div style="width: 50%; height: 100%; float: left;"> 
+            <div style="width: 25%; height: 100%; float: left;"> 
                 <img style="height: 100%;  display: flex; justify-content: center; align-items: center;" src="https://media-exp1.licdn.com/dms/image/C5603AQFvAaQW30mjag/profile-displayphoto-shrink_400_400/0/1629331853308?e=1674086400&v=beta&t=z62IE1kzoF_2Y7N9iEL5oxM1gHSzvdnoPFrysVoxZ3g" alt="Nicole">
             </div>
             
-            <div style="width: 50%; height: 100%; float: left;"> 
+            <div style="width: 75%; height: 100%; float: left;"> 
 
                 <p style="font-size:25px; color: #372823; background-color: #fde3c8;text-align: center;">
                     Nicole Goral is a senior studying Management Information Systems and International Business. 
@@ -46,9 +46,9 @@
 
         </div>
 
-        <div style="width: 100%; height:300px;  margin-top: 25px; margin-bottom: 25px; background-color: #fde3c8; color:#372823; ">
+        <div style="width: 100%; height:300px; margin-bottom: 10px; background-color: #fde3c8; color:#372823; ">
             
-            <div style="width: 50%; height: 100%; float: left;"> 
+            <div style="width: 75%; height: 100%; float: left;"> 
                 <p style="font-size:25px; color: #372823; background-color: #fde3c8;text-align: center;">
                 Darren Fisher is a senior studying Management Information Systems and is on the accelerated Masters track in Information Technology. 
                         Darren is constantly trying to create the next big thing or work with startups. 
@@ -56,7 +56,7 @@
                 </p>
             </div>
 
-            <div style="width: 50%; height: 100%; float: left;text-align: center; "> 
+            <div style="width: 25%; height: 100%; float: left;text-align: center; "> 
                 <img style="height: 100%; text-align: center; display: flex; justify-content: center; align-items: center;" src="https://media-exp1.licdn.com/dms/image/C4E03AQEXLkjEQK7QYg/profile-displayphoto-shrink_400_400/0/1621025060836?e=1674086400&v=beta&t=8iPq0SLTLrraWpaoK4p6oY-euPQlglgDJSPuNzjlu0c" alt="Darren">
             </div>
 
