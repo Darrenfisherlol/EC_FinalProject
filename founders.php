2a2c3fc0db68cb0260a1e742bb1ebbc2c6128d51
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-12-08 20:30:42 -0600

    Add files via upload

--- founders.php
+++ founders.php
@@ -30,8 +30,8 @@
          
         <div style="width: 100%; height:300px;  margin-top: 25px; margin-bottom: 25px; background-color: #fde3c8; color:#372823; ">
         
-            <div style="width: 50%; height: 100%; float: left;"> 
-                <img style="height: 100%; text-align: center; display: flex; justify-content: center; align-items: center;" src="https://media-exp1.licdn.com/dms/image/C5603AQFvAaQW30mjag/profile-displayphoto-shrink_400_400/0/1629331853308?e=1674086400&v=beta&t=z62IE1kzoF_2Y7N9iEL5oxM1gHSzvdnoPFrysVoxZ3g" alt="Nicole">
+            <div style="width: 50%; height: 100%; float: left; text-align: center; margin-left:25%"> 
+                <img style="height: 100%;  display: flex; justify-content: center; align-items: center;" src="https://media-exp1.licdn.com/dms/image/C5603AQFvAaQW30mjag/profile-displayphoto-shrink_400_400/0/1629331853308?e=1674086400&v=beta&t=z62IE1kzoF_2Y7N9iEL5oxM1gHSzvdnoPFrysVoxZ3g" alt="Nicole">
             </div>
             
             <div style="width: 50%; height: 100%; float: left;"> 
@@ -48,7 +48,7 @@
 
         <div style="width: 100%; height:300px;  margin-top: 25px; margin-bottom: 25px; background-color: #fde3c8; color:#372823; ">
         
-        <div style="width: 50%; height: 100%; float: left;"> 
+        <div style="width: 50%; height: 100%; float: left;text-align: center; margin-left:25%"> 
             <img style="height: 100%; text-align: center; display: flex; justify-content: center; align-items: center;" src="https://media-exp1.licdn.com/dms/image/C4E03AQEXLkjEQK7QYg/profile-displayphoto-shrink_400_400/0/1621025060836?e=1674086400&v=beta&t=8iPq0SLTLrraWpaoK4p6oY-euPQlglgDJSPuNzjlu0c" alt="Darren">
         </div>
         
