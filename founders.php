6ed15cf1d3a92b5c1496ca18c819513f1ae30950
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-12-08 20:32:45 -0600

    Add files via upload

--- founders.php
+++ founders.php
@@ -30,7 +30,7 @@
          
         <div style="width: 100%; height:300px;  margin-top: 25px; margin-bottom: 25px; background-color: #fde3c8; color:#372823; ">
         
-            <div style="width: 50%; height: 100%; float: left; text-align: center; margin-left:25%"> 
+            <div style="width: 50%; height: 100%; float: left; text-align: center;"> 
                 <img style="height: 100%;  display: flex; justify-content: center; align-items: center;" src="https://media-exp1.licdn.com/dms/image/C5603AQFvAaQW30mjag/profile-displayphoto-shrink_400_400/0/1629331853308?e=1674086400&v=beta&t=z62IE1kzoF_2Y7N9iEL5oxM1gHSzvdnoPFrysVoxZ3g" alt="Nicole">
             </div>
             
@@ -47,22 +47,20 @@
         </div>
 
         <div style="width: 100%; height:300px;  margin-top: 25px; margin-bottom: 25px; background-color: #fde3c8; color:#372823; ">
-        
-        <div style="width: 50%; height: 100%; float: left;text-align: center; margin-left:25%"> 
-            <img style="height: 100%; text-align: center; display: flex; justify-content: center; align-items: center;" src="https://media-exp1.licdn.com/dms/image/C4E03AQEXLkjEQK7QYg/profile-displayphoto-shrink_400_400/0/1621025060836?e=1674086400&v=beta&t=8iPq0SLTLrraWpaoK4p6oY-euPQlglgDJSPuNzjlu0c" alt="Darren">
+            
+            <div style="width: 50%; height: 100%; float: left;"> 
+                <p style="font-size:25px; color: #372823; background-color: #fde3c8;text-align: center;">
+                Darren Fisher is a senior studying Management Information Systems and is on the accelerated Masters track in Information Technology. 
+                        Darren is constantly trying to create the next big thing or work with startups. 
+                        Darren loves helping his friends and creating solutions to problems even more!
+                </p>
+            </div>
+
+            <div style="width: 50%; height: 100%; float: left;text-align: center; "> 
+                <img style="height: 100%; text-align: center; display: flex; justify-content: center; align-items: center;" src="https://media-exp1.licdn.com/dms/image/C4E03AQEXLkjEQK7QYg/profile-displayphoto-shrink_400_400/0/1621025060836?e=1674086400&v=beta&t=8iPq0SLTLrraWpaoK4p6oY-euPQlglgDJSPuNzjlu0c" alt="Darren">
+            </div>
+
         </div>
-        
-        <div style="width: 50%; height: 100%; float: left;"> 
-
-            <p style="font-size:25px; color: #372823; background-color: #fde3c8;text-align: center;">
-            Darren Fisher is a senior studying Management Information Systems and is on the accelerated Masters track in Information Technology. 
-                    Darren is constantly trying to create the next big thing or work with startups. 
-                    Darren loves helping his friends and creating solutions to problems even more!
-            </p>
-
-        </div>
-
-    
 
     </body>
 </html>
