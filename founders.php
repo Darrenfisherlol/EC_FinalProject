ec3b6f715e2c435d8341108568c3c2dd64a8dc6c
Nicole Goral <78056391+NicoleGoral@users.noreply.github.com>
2022-12-14 20:59:58 -0600

    Update founders.php

--- founders.php
+++ founders.php
@@ -14,7 +14,7 @@
             </p>
         </div>
 
-        <div style="width: 100%; height:175px; margin-bottom: 10px;; background-color: #f3ce8b; color:#372823 ">
+        <div style="width: 100%; height:300px; margin-bottom: 20px;; background-color: #f3ce8b; color:#372823 ">
             
             <div style="width: 31%; float: left;font-size:25px; margin-top: 10px; text-align:center; margin-left:15px; margin-right:15px; background-color: #fde3c8;">
                 Nicole and Darren worked with the University of Oklahoma, Dr. Bellah, and OU create to make all their dreams come true.
