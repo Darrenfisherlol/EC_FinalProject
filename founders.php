fd212976b39468569095930ac42e20350db4a2a8
Nicole Goral <78056391+NicoleGoral@users.noreply.github.com>
2022-12-14 21:16:00 -0600

    Update founders.php

--- founders.php
+++ founders.php
@@ -16,13 +16,13 @@
 
         <div style="width: 100%; height:200px; margin-bottom: 10px;; background-color: #f3ce8b; color:#372823 ">
             
-            <div style="width: 31%; float: left;font-size:25px; margin-top: 10px; text-align:center; margin-left:15px; margin-right:15px; background-color: #fde3c8;">
+            <div style="width: 31%; float: left;font-size:25px; margin-top: 30px; text-align:center; margin-left:15px; margin-right:15px; background-color: #fde3c8;">
                 Nicole and Darren worked with the University of Oklahoma, Dr. Bellah, and OU create to make all their dreams come true.
             </div>
-            <div style="width: 31%; float: left;font-size:25px;margin-top: 10px; text-align:center; margin-left:15px; margin-right:15px;background-color: #fde3c8;">
+            <div style="width: 31%; float: left;font-size:25px;margin-top: 30px; text-align:center; margin-left:15px; margin-right:15px;background-color: #fde3c8;">
                 They hope that you have a wonderful experience using Jobly!
             </div>
-            <div style="width: 31%; float: left;font-size:25px; margin-top: 10px;text-align:center; margin-left:15px; margin-right:15px;background-color: #fde3c8;">
+            <div style="width: 31%; float: left;font-size:25px; margin-top: 30px;text-align:center; margin-left:15px; margin-right:15px;background-color: #fde3c8;">
                 Jobly is their beautiful brain child that they hope will help future students and job seekers to stay organized and eventually find a job.
             </div>
           
@@ -46,7 +46,7 @@
 
         </div>
 
-        <div style="width: 100%; height:300px; margin-bottom: 10px; background-color: #fde3c8; color:#372823; ">
+        <div style="width: 100%; height:300px; margin-bottom: 10px; background-color: #f3ce8b; color:#372823; ">
             
             <div style="width: 75%; height: 100%; float: left;text-align: center;"> 
 
